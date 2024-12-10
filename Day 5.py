3ccdfa8581b09407856a11f527c9095c0c62b083
ssapre2 <30734739+ssapre2@users.noreply.github.com>
2024-12-09 21:50:21 -0800

    Almost!
    Only 150 off, but I'm close!

--- Day 5.py
+++ Day 5.py
@@ -65,6 +65,8 @@ class TextGrid:
     # Now let's traverse this grid in search of the 'X', our starting point
     def find_next_x(self):
 
+        possible_x = range(-1,2,1)
+        possible_y = range(-1,2,1)
 
         # Loop thru grid to find X's
         while self.row < self.text_grid.shape[0] and self.col < self.text_grid.shape[1]:
@@ -74,31 +76,59 @@ class TextGrid:
             if self.current_letter == 'X':
                 print(f"X at ({self.row+1},{self.col+1})")
                 self.x_positions.append((self.row,self.col))
+                # Add X to word list
+                self.word_list.append('X')
+                self.letter_count+=1
+                # Try all direction combinations
+                possible_directions = [(x,y) for x in possible_x for y in possible_y]
+                # Apply 'find_word' function to list
+                xmas_finds = [self.find_word(x = self.row, y= self.col,x_dir=  direction[0],y_dir = direction[1]) for direction in possible_directions]
             
+                # Add to count 
+                self.xmas_count += len(xmas_finds)
             # if it's the end of the row, move down 1 row and reset column index
             if self.col >= self.text_grid.shape[1]-1:
                 self.row += 1
-                self.col = 0
+                self.col = 0           
+                self.word_list.clear()
+                self.letter_count = 0
+
             else:
                 self.col += 1
+                self.word_list.clear()
+                self.letter_count = 0
+
     
          
 
     def find_word(self,x,y,x_dir, y_dir):
+        print(f"Direction ({x_dir},{y_dir})")
 
-        if self.letter_count == 4:
+        # If target hits
+        if  self.word_list == self.target:
             print(self.word_list)
             return(self.word_list)
+        # Check that we're on the way the 'XMAS'
+        elif self.word_list != self.target[len(self.word_list):]: 
+            # Clear word list tracker
+            print(f"Not on track w/ {self.word_list}")
+
         else:
             x += x_dir
             y += y_dir
-            next_letter = self.text_grid[x,y]
-            self.letter_count += 1
-            print(self.letter_count)
-            # Add letter to word count
-            self.word_list.append(next_letter)
-            # Recurse
-            self.find_word(x,y,x_dir,y_dir)
+            try:
+                next_letter = self.text_grid[x,y]
+                print(f"Next Letter {next_letter}")
+                self.letter_count += 1
+                print(self.letter_count)
+                # Add letter to word count
+                self.word_list.append(next_letter)
+                # Recurse
+                self.find_word(x,y,x_dir,y_dir)
+            except Exception as E:
+                print(f"Index ({x,y}) Not in test grid")
+
+
 
 
         
@@ -114,7 +144,9 @@ def main():
 # Main body of code. Other functions and class methods are called from main.
     obj = TextGrid('sample_text.txt')
     obj.find_next_x()
-    obj.find_word(x = obj.x_positions[0][0],y = obj.x_positions[0][1],x_dir=-1,y_dir=0)
+    print(obj.xmas_count)
+
+    #obj.find_word(x = obj.x_positions[0][0],y = obj.x_positions[0][1],x_dir=-1,y_dir=0)
 
 if __name__ == "__main__":
     main()
