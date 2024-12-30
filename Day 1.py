ba6f77073231a1e476e288bdac782166c2e21517
ssapre2 <30734739+ssapre2@users.noreply.github.com>
2024-12-30 22:57:22 +0530

    open file
    read data from day 1

---
+++ Day 1.py
@@ -0,0 +1,12 @@
+def sort():
+    pass
+
+
+def main():
+    with open("input_day1.txt", "r") as file:
+        lines = file.readlines()  # Returns a list of lines
+    print(lines)
+
+
+if __name__ == "__main__":
+    main()
