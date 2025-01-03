13a657148b349e61fa54facb4cb61a34ef8c2ab9
ssapre2 <30734739+ssapre2@users.noreply.github.com>
2025-01-03 05:31:17 -0500

    Update Day 1.py
    finish day 1

--- Day 1.py
+++ Day 1.py
@@ -1,11 +1,32 @@
+import pandas as np
+import numpy as np
+
 def sort():
     pass
 
 
 def main():
+
+    sim_dict = dict()
     with open("input_day1.txt", "r") as file:
-        lines = file.readlines()  # Returns a list of lines
-    print(lines)
+        #lines = file.readlines()  # Returns a list of lines
+        left = []
+        right = []
+        for line in file:
+        # Split each line into two parts
+            col1, col2 = line.split()
+            left.append(int(col1))                
+            right.append(int(col2))
+            left.sort()
+            right.sort()
+    diffs = [abs(left[i] - right[i]) for i in range(0,len(left))]
+    sims = [left[i] * len(list(filter(lambda x: x  == left[i], right))) for i in range(0,len(left))]
+    diffs.sort()
+
+    
+
+    print(sum(diffs))
+    print(f"Similarity Score: {sum(sims)}")
 
 
 if __name__ == "__main__":
